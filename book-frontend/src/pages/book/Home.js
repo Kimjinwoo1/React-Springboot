1f8005160b974b10d29b52e9ffe9adaffd8666af
kim jin woo <wlsdn090909@naver.com>
2024-02-23 17:00:10 +0900

    백엔드 데이터를 받아와서 React 화면에 뿌리기

--- book-frontend/src/pages/book/Home.js
+++ book-frontend/src/pages/book/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BookItem from '../../components/BookItem'
 
 const Home = () => {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:8080/book")
+            .then(res => res.json())
+            .then(res => {
+                console.log(1, res)
+                setBooks(res)
+            });
+    }, [])
+
     return (
         <div>
-            <BookItem />
+            {books.map((book) => (<BookItem />))}
         </div>
     )
 }
