6b8d1f05d6c85b8125b3610b02e5df07a7fab032
kim jin woo <wlsdn090909@naver.com>
2024-02-28 19:39:00 +0900

    updateForm 페이지 UI & 기능 완료

--- book-frontend/src/pages/book/UpdateForm.js
+++ book-frontend/src/pages/book/UpdateForm.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const UpdateForm = () => {
+
+    const { id } = useParams()
+
+    const navigate = useNavigate()
+
+    const [book, setBook] = useState({
+        title: "",
+        author: "",
+    })
+
+    useEffect(() => {
+        fetch("http://localhost:8080/book/" + id)
+            .then(res => res.json())
+            .then(res => {
+                setBook(res)
+            })
+    }, [id])
+
+
+    const changeValue = (e) => {
+        setBook({
+            ...book,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submitBook = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch("http://localhost:8080/book/" + id, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(book)
+            });
+
+            if (res.status === 200) {
+                const data = await res.json();
+                navigate('/book/' + id)
+            } else {
+                alert('책 수정에 실패하였습니다.')
+            }
+        } catch (error) {
+            alert("책 수정에 실패하였습니다.")
+        }
+    }
+
     return (
-        <div>
-            <h1>책 수정하기</h1>
-        </div>
+        <Form onSubmit={submitBook}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter Title" onChange={changeValue} name="title" value={book.title} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Author</Form.Label>
+                <Form.Control type="text" placeholder="Enter Author" onChange={changeValue} name="author" value={book.author} />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     )
 }
 
